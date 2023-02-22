242fb8dc15146a55c3a5f282f7e077f1f3f5db83
Yash Shah <yash7june@gmail.com>
2023-02-23 00:07:09 +0530

    first commit

--- registration.php
+++ registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
  
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'MyVendor_BasicModule',
+    'Yashshah_BasicModule',
     __DIR__
 );
